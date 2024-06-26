ba93d2682a7c8fcc20a8654a22e1eea1d6d34cc1
Bulsch <155096635+Bulsch@users.noreply.github.com>
2024-06-26 11:58:49 +0200

    Add function 'getHumanChoice'

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 100) + 1 ;
     let choice;
     if (random <= 33){
-        choice = "Rock";
+        choice = "rock";
     } else if(34 <= random && random <= 67){
-        choice = "Paper";
+        choice = "paper";
     } else {
-        choice = "Scissors";
+        choice = "scissors";
     }
-    console.log(random);
+
+ //   console.log(random);
     return choice;
 
 }
 
+function getHumanChoice() {
+    let choice = prompt("Rock, paper or scissors?").toLowerCase();
+    if (choice === "rock" || choice ==='paper' || choice === 'scissors'){
+        return choice;
+    }
+
+    alert("Stick to rock, paper or scissors!");
+}
