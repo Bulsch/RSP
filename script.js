deef5c70718f8cb324cbfa78d79b8c1457e0cdd5
Bulsch <155096635+Bulsch@users.noreply.github.com>
2024-06-26 11:41:33 +0200

    Add function 'getComputerChoice'

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("yeah");
+
+
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 100) + 1 ;
+    let choice;
+    if (random <= 33){
+        choice = "Rock";
+    } else if(34 <= random && random <= 67){
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    console.log(random);
+    return choice;
+
+}
+
